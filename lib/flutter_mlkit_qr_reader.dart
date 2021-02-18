66b69556fdfb6e885064c9695891b3d75e7af988
dev <denys.loziuk@uapay.ua>
2021-02-18 12:04:06 +0200

    add android code

--- lib/flutter_mlkit_qr_reader.dart
+++ lib/flutter_mlkit_qr_reader.dart
@@ -1,12 +1,10 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-class FlutterMlkitQrReader {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_mlkit_qr_reader');
+const _channel = MethodChannel('flutter_mlkit_qr_reader');
 
+class FlutterMlkitQrReader {
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
